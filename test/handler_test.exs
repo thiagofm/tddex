50bd9b5630483f15f930657a8b08d24322250ce4
Thiago Fernandes Massa <thiagown@gmail.com>
2019-05-27 23:42:38 +0200

    Still figuring out how to do this Handler thingy

--- test/handler_test.exs
+++ test/handler_test.exs
@@ -4,6 +4,9 @@ defmodule Tddex.HandlerTest do
   alias Tddex.Handler
 
   test "file_event/1" do
+   {:ok, _fd, file_path} = Temp.open "test-file"
+
+    assert Handler.file_event(file_path) == file_path #TODO: must implement that
   end
 end
 
