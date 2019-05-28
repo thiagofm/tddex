069cf25fc67439fcc1343d5db4b1993fe2aea901
Thiago Fernandes Massa <thiagown@gmail.com>
2019-05-28 21:12:24 +0200

    Hmm, will probably need mox

--- lib/handler.ex
+++ lib/handler.ex
@@ -2,6 +2,7 @@ defmodule Tddex.Handler do
   alias Tddex.Runner
 
   def file_event(file_path) do
+    IO.inspect "Test changed: #{file_path}"
     # Hm... Modes.
     # 1. Run all test files when a file is saved can be a start.
     # 2. Try to find a test for the file saved.
