eb434c3696e1fb6e5a8cddaea3a60ac4327007c6
Thiago Fernandes Massa <thiagown@gmail.com>
2019-06-11 22:39:10 +0200

    Removing useless code

--- lib/tddex.ex
+++ lib/tddex.ex
@@ -2,17 +2,4 @@ defmodule Tddex do
   @moduledoc """
   Documentation for Tddex.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tddex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/tddex_test.exs
+++ test/tddex_test.exs
@@ -1,4 +1,3 @@
 defmodule TddexTest do
   use ExUnit.Case
-  doctest Tddex
 end
